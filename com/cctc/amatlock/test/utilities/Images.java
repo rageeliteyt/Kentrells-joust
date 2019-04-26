c294446f8ffea0a53a60c8c5814583f05a967abf
rageeliteyt <49986872+rageeliteyt@users.noreply.github.com>
2019-04-26 08:54:40 -0500

    Add files via upload

---
+++ com/cctc/amatlock/test/utilities/Images.java
@@ -0,0 +1,14 @@
+package com.cctc.amatlock.test.utilities;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * This class will just hold all the image variables
+ * used in our game. It is a purely organizational
+ * tool.
+ */
+public class Images
+{
+    // This is the image we are loading.
+    public static BufferedImage background;
+}
