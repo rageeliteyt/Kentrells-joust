5bddff8574be067ae1184af0e4fa1a05a5f599d1
rageeliteyt <49986872+rageeliteyt@users.noreply.github.com>
2019-05-01 09:56:05 -0500

    Add files via upload

--- com/cctc/amatlock/test/utilities/ResourceLoader.java
+++ com/cctc/amatlock/test/utilities/ResourceLoader.java
@@ -7,7 +7,7 @@ public class ResourceLoader
     // Creates an instance of ImageLoader
     // This is used to load each image
     private static ImageLoader imageLoader = new ImageLoader();
-
+    private static SoundLoader soundLoader = new SoundLoader();
     /**
      * This method will load and store every image
      * we plan to use in our program. This is the
@@ -15,12 +15,32 @@ public class ResourceLoader
      * add more images.
      */
     public static void loadImages()
+{
+
+
+    // The try/catch is code to catch if
+    // any image isn't found.
+    try{
+        Images.background = imageLoader.loadImage("background.png");
+    } catch (IOException e)
     {
+        e.printStackTrace();
+    }
+}
+
+
+
+
+
+    public static void loadSounds()
+    {
+
+
         // The try/catch is code to catch if
         // any image isn't found.
         try{
-            Images.background = imageLoader.loadImage("background.png");
-        } catch (IOException e)
+            Sounds.background = soundLoader.loadSound("background.wav");
+        } catch (Exception e)
         {
             e.printStackTrace();
         }
